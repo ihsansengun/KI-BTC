b55444e1afcb551ddebcf9c743e806d1482f381c
Ihsan <me@ihsansengun.com>
2017-08-08 15:02:52 +0100

    Completed reveal on scroll.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,12 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import  $ from 'jquery';
+
 
 var mobileMenu = new MobileMenu();
+
+new RevealOnScroll($('.colored-icon-list__item'), '85%');
+new RevealOnScroll($('.course-box ul li'), '65%');
+
+
+
